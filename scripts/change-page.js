5f113508445c4aa34524e4de08caf41270ec6670
Deilton Pedro <74920632+DeJunior007@users.noreply.github.com>
2022-12-25 10:44:08 -0300

    JS para mudança de pagina
    Pasta dos arquivos JS + arquivo js para mudança de pagina

---
+++ scripts/change-page.js
@@ -0,0 +1,18 @@
+function goStartPage() {
+    location.href = "/start.html"
+};
+function goHomePage() {
+location.href = "/Home.html"
+};
+function goForms1() {
+location.href = "/Forms-1.html"
+};
+function goForms2() {
+    location.href = "/Forms-2.html"
+};
+function goForms3() {
+location.href = "/Forms-3.html"
+};
+function goSign() {
+location.href = "/sign.html"
+};
